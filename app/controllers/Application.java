47e09d3867fa6c8ea8bd12a74fea708b9a3d54ab
Iaron da Costa Araujo <iaron.araujo@ccc.ufcg.edu.br>
2016-07-18 17:49:47 -0300

    Natan's modifications: increased the Application class, modified index and main, created cadastro and home classes and modified the routes.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,12 +1,30 @@
 package controllers;
 
-import play.mvc.Controller;
-import play.mvc.Result;
+import models.User;
+import play.mvc.*;
+import views.html.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application extends Controller {
-    
+
+    private List<User> listUsers = new ArrayList<>();
+
     public static Result index() {
-        return ok(views.html.index.render("Hello Play Framework"));
+        return ok(views.html.index.render("Green Box"));
     }
-    
+
+    public static Result cadastro() {
+        return ok(cadastro.render("Cadastro"));
+    }
+
+    public static  Result login() {
+        return ok(home.render());
+    }
+
+
+
+
+
 }

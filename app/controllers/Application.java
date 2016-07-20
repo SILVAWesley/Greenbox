01095b13d168b9d71a6eca9536f1322c42fcf5c4
Natan Macena Ribeiro <natan.ribeiro@ccc.ufcg.edu.br>
2016-07-20 14:00:51 -0300

    1ª tentativa de fazer ligações antre html e application para realizar cadastro

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,30 +1,34 @@
 package controllers;
 
 import models.User;
+import play.data.FormFactory;
 import play.mvc.*;
 import views.html.*;
 
+import javax.inject.Inject;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Application extends Controller {
 
+    @Inject
+    private FormFactory formFactory;
     private List<User> listUsers = new ArrayList<>();
 
-    public static Result index() {
-        return ok(views.html.index.render("Green Box"));
+    public Result index() {
+        return ok(index.render("Green Box"));
     }
 
-    public static Result cadastro() {
-        return ok(cadastro.render("Cadastro"));
+    public Result cadastrarUser() {
+        User user = formFactory.form(User.class).bindFromRequest().get();
+        listUsers.add(user);
+        return redirect(routes.Application.login());
+        //User user = new User("dsf","asdas","sadas");
+        //return ok(cadastro.render(user));
     }
 
-    public static  Result login() {
+    public Result login() {
         return ok(home.render());
     }
-
-
-
-
 
 }
